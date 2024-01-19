dfc92bd4b82af98c1d51c8da2882996608110498
dn320 <dn320@ic.ac.uk>
2024-01-19 21:00:37 +0000

    [Feature] Setup basic electron vite app

---
+++ e7-havoc-calc/src/renderer/src/App.tsx
@@ -0,0 +1,36 @@
+import '@mantine/core/styles.css';
+import { MantineProvider, AppShell, Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+
+function App(): JSX.Element {
+  const [opened, { toggle }] = useDisclosure();
+  return <MantineProvider>
+    {
+      <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: 'sm',
+        collapsed: { mobile: !opened },
+      }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Burger
+          opened={opened}
+          onClick={toggle}
+          hiddenFrom="sm"
+          size="sm"
+        />
+        <div>Logo</div>
+      </AppShell.Header>
+
+      <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+
+      <AppShell.Main>Main</AppShell.Main>
+    </AppShell>
+    }
+  </MantineProvider>
+}
+
+export default App
